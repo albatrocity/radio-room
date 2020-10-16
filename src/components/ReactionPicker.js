756e7594e99294208117346bc5a867cf1ea76191
Ross Brown <albatrocity@gmail.com>
2020-10-15 21:23:06 -0500

    Uses brand color for emoji picker

--- src/components/ReactionPicker.js
+++ src/components/ReactionPicker.js
@@ -1,16 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import "emoji-mart/css/emoji-mart.css"
 import data from "emoji-mart/data/apple.json"
 import { NimblePicker } from "emoji-mart"
+import { ThemeContext } from "grommet"
 
-const ReactionPicker = ({ onSelect }) => (
-  <NimblePicker
-    autoFocus={true}
-    title={""}
-    data={data}
-    set="apple"
-    onSelect={onSelect}
-  />
-)
+const ReactionPicker = ({ onSelect }) => {
+  const theme = useContext(ThemeContext)
+  return (
+    <NimblePicker
+      autoFocus={true}
+      title={""}
+      data={data}
+      set="apple"
+      onSelect={onSelect}
+      color={theme.global.colors.brand}
+    />
+  )
+}
 
 export default ReactionPicker
