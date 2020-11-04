ecf0ae6d562f6d9353ad396600bbaa033e06c069
Ross Brown <albatrocity@gmail.com>
2020-11-04 17:55:36 -0600

    changes page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Koney Live" />
+    <SEO title="Ross' Dumb Radio Show" />
     <Helmet>
       <meta
         name="viewport"
