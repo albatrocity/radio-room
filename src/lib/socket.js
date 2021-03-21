bcebdb3e6a8ea7dfb5c40fa3c48c2bd742c72ba0
Ross Brown <albatrocity@gmail.com>
2021-03-21 11:46:15 -0500

    attempts websocket first

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,5 +1,7 @@
 import socketIOClient from "socket.io-client"
 const socketEndPoint = process.env.GATSBY_API_URL
-const socket = socketIOClient(socketEndPoint)
+const socket = socketIOClient(socketEndPoint, {
+  transports: ["websocket", "polling"],
+})
 
 export default socket
