ad6be3855a2e28b966b3e38114581e73af2f5d0c
Ross Brown <albatrocity@gmail.com>
2023-02-27 13:53:30 -0600

    moves add to queue button to NowPlaying

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useMemo } from "react"
-import { FiUser, FiMusic } from "react-icons/fi"
+import { FiUser, FiMusic, FiPlus } from "react-icons/fi"
 import {
   BoxProps,
   Box,
@@ -13,6 +13,7 @@ import {
   Stack,
   Icon,
   Hide,
+  Button,
 } from "@chakra-ui/react"
 
 import AlbumArtwork from "./AlbumArtwork"
@@ -43,6 +44,11 @@ const NowPlaying = ({ offline, meta }: NowPlayingProps) => {
     dj,
   } = meta || {}
   const djUsername = users.find(({ userId }) => userId === dj)?.username
+  const [roomState] = useActor(globalServices.roomService)
+  const isDeputyDj = roomState.matches("deputyDjaying.isDj")
+  const isDj = roomState.matches("djaying")
+  const canDj = isDeputyDj || isDj
+
   const { mbid, releaseDate } = release || {}
   const releaseUrl = release?.url
     ? release.url
@@ -67,85 +73,102 @@ const NowPlaying = ({ offline, meta }: NowPlayingProps) => {
       alignItems="center"
       justifyContent="center"
       flexGrow={1}
+      height="100%"
     >
-      {offline ? (
-        <VStack>
-          <Heading
-            margin="none"
-            as="h2"
-            size="lg"
-            color="white"
-            textAlign="center"
-          >
-            Offline :(
-          </Heading>
-          <Hide above="sm">
-            <ButtonListeners />
-          </Hide>
-        </VStack>
-      ) : (
-        <LinkBox>
-          <Stack direction={["row", "column"]} spacing={5} justify="center">
-            {coverUrl ? (
-              <Box
-                width={artworkSize}
-                height={artworkSize}
-                flex={{ shrink: 0, grow: 1 }}
-              >
-                <AlbumArtwork coverUrl={coverUrl} />
-              </Box>
-            ) : (
-              <Flex
-                background="primaryBg"
-                width={artworkSize}
-                height={artworkSize}
-                align="center"
-                justify="center"
-                flex={{ shrink: 0, grow: 1 }}
-              >
-                <Icon as={FiMusic} boxSize={20} color="primary.500" />
-              </Flex>
-            )}
-            <VStack align={"start"} spacing={0}>
-              {(track || title) && (
-                <LinkOverlay href={releaseUrl} target="_blank">
-                  <Heading
-                    color="primaryBg"
-                    margin="none"
-                    as="h3"
-                    size={["md", "lg"]}
-                  >
-                    {track || title.replace(/\|/g, "")}
+      <VStack spacing={2} justify="space-between" height="100%">
+        {offline ? (
+          <VStack>
+            <Heading
+              margin="none"
+              as="h2"
+              size="lg"
+              color="white"
+              textAlign="center"
+            >
+              Offline :(
+            </Heading>
+            <Hide above="sm">
+              <ButtonListeners />
+            </Hide>
+          </VStack>
+        ) : (
+          <LinkBox>
+            <Stack
+              direction={["row", "column"]}
+              spacing={5}
+              justify="center"
+              flexGrow={1}
+            >
+              {coverUrl ? (
+                <Box
+                  width={artworkSize}
+                  height={artworkSize}
+                  flex={{ shrink: 0, grow: 1 }}
+                >
+                  <AlbumArtwork coverUrl={coverUrl} />
+                </Box>
+              ) : (
+                <Flex
+                  background="primaryBg"
+                  width={artworkSize}
+                  height={artworkSize}
+                  align="center"
+                  justify="center"
+                  flex={{ shrink: 0, grow: 1 }}
+                >
+                  <Icon as={FiMusic} boxSize={20} color="primary.500" />
+                </Flex>
+              )}
+              <VStack align={"start"} spacing={0}>
+                {(track || title) && (
+                  <LinkOverlay href={releaseUrl} target="_blank">
+                    <Heading
+                      color="primaryBg"
+                      margin="none"
+                      as="h3"
+                      size={["md", "lg"]}
+                    >
+                      {track || title.replace(/\|/g, "")}
+                    </Heading>
+                  </LinkOverlay>
+                )}
+                {artist && (
+                  <Heading color="primaryBg" margin="none" as="h4" size="sm">
+                    {artist}
                   </Heading>
-                </LinkOverlay>
-              )}
-              {artist && (
-                <Heading color="primaryBg" margin="none" as="h4" size="sm">
-                  {artist}
-                </Heading>
-              )}
-              {album && (
-                <Text as="span" color="primaryBg" margin="none" fontSize="xs">
-                  {album}
-                </Text>
-              )}
-              {releaseDate && (
-                <Text as="span" color="primaryBg" fontSize="xs">
-                  Released {safeDate(releaseDate)}
-                </Text>
-              )}
-              {dj && (
-                <HStack mt={4} spacing={2}>
-                  <Icon color="primaryBg" boxSize={3} as={FiUser} />
-                  <Text as="i" color="primaryBg" fontSize="xs">
-                    Added by {djUsername}
+                )}
+                {album && (
+                  <Text as="span" color="primaryBg" margin="none" fontSize="xs">
+                    {album}
                   </Text>
-                </HStack>
-              )}
-            </VStack>
-          </Stack>
-        </LinkBox>
-      )}
+                )}
+                {releaseDate && (
+                  <Text as="span" color="primaryBg" fontSize="xs">
+                    Released {safeDate(releaseDate)}
+                  </Text>
+                )}
+                {dj && (
+                  <HStack mt={4} spacing={2}>
+                    <Icon color="primaryBg" boxSize={3} as={FiUser} />
+                    <Text as="i" color="primaryBg" fontSize="xs">
+                      Added by {djUsername}
+                    </Text>
+                  </HStack>
+                )}
+              </VStack>
+            </Stack>
+          </LinkBox>
+        )}
+        {canDj && (
+          <Button
+            leftIcon={<Icon as={FiMusic} />}
+            rightIcon={<Icon as={FiPlus} />}
+            onClick={() => globalServices.roomService.send("EDIT_QUEUE")}
+          >
+            Add to queue
+          </Button>
+        )}
+      </VStack>
     </Box>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,6 @@
 import React from "react"
-import {
-  Box,
-  Stack,
-  Flex,
-  IconButton,
-  Icon,
-  Show,
-  Button,
-} from "@chakra-ui/react"
-import { useSelector, useActor } from "@xstate/react"
+import { Box, Stack, Flex, IconButton, Icon, Show } from "@chakra-ui/react"
+import { useSelector } from "@xstate/react"
 import { FiHelpCircle } from "react-icons/fi"
 import Listeners from "./Listeners"
 import useGlobalContext from "./useGlobalContext"
@@ -28,9 +20,6 @@ const Sidebar = (props: Props) => {
   )
 
   const isAdmin = useSelector(globalServices.authService, isAdminSelector)
-  const [state] = useActor(globalServices.roomService)
-
-  const isDeputyDj = state.matches("deputyDjaying.isDj")
 
   return (
     <Box
@@ -65,13 +54,6 @@ const Sidebar = (props: Props) => {
           />
         </Flex>
         <Show above="sm">
-          {isDeputyDj && (
-            <Button
-              onClick={() => globalServices.roomService.send("EDIT_QUEUE")}
-            >
-              Add to queue
-            </Button>
-          )}
           {!isAdmin && (
             <Flex p={3} align="center" grow={1} shrink={0}>
               <IconButton
