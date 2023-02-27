a1565a7d513caf9440b9374548df7016fe89c3c2
Ross Brown <albatrocity@gmail.com>
2023-02-27 15:01:23 -0600

    persists theme choice

--- src/machines/themeMachine.ts
+++ src/machines/themeMachine.ts
@@ -1,4 +1,5 @@
 import { Machine, assign } from "xstate"
+import session from "sessionstorage"
 
 export const themeMachine = Machine(
   {
@@ -6,9 +7,10 @@ export const themeMachine = Machine(
     context: {
       theme: "default",
     },
+    entry: ["loadTheme"],
     on: {
       SET_THEME: {
-        actions: ["setTheme"],
+        actions: ["setTheme", "persistTheme"],
       },
     },
   },
@@ -16,6 +18,12 @@ export const themeMachine = Machine(
     actions: {
       setTheme: assign({
         theme: (context, event) => event.theme,
+      }),
+      persistTheme: (context) => {
+        session.setItem("theme", context.theme)
+      },
+      loadTheme: assign({
+        theme: (context) => sessionStorage.getItem("theme") || "default",
       }),
     },
   },
