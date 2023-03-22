4666de394f265e1ce934e3f9db81d30347417a63
Ross Brown <ross@crema.us>
2023-03-22 11:22:38 -0500

    uses ColorHues type in AppTheme

--- src/types/AppTheme.ts
+++ src/types/AppTheme.ts
@@ -1,42 +1,11 @@
+import { ColorHues } from "@chakra-ui/react"
+
 export interface AppTheme {
   name: string
   id: string
   colors: {
-    primary: {
-      50: string
-      100: string
-      200: string
-      300: string
-      400: string
-      500: string
-      600: string
-      700: string
-      800: string
-      900: string
-    }
-    secondary: {
-      50: string
-      100: string
-      200: string
-      300: string
-      400: string
-      500: string
-      600: string
-      700: string
-      800: string
-      900: string
-    }
-    action: {
-      50: string
-      100: string
-      200: string
-      300: string
-      400: string
-      500: string
-      600: string
-      700: string
-      800: string
-      900: string
-    }
+    primary: ColorHues
+    secondary: ColorHues
+    action: ColorHues
   }
 }
