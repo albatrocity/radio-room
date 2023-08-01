81a314ce61edbc1d8beaaf7117ae18ecc796c16b
Ross Brown <albatrocity@gmail.com>
2023-08-01 08:54:36 -0500

    sets XHR headers in Howler

--- src/components/RadioPlayer.tsx
+++ src/components/RadioPlayer.tsx
@@ -186,6 +186,10 @@ const RadioPlayer = ({
           onPlay={onPlay}
           onLoad={onLoad}
           autoSuspend={false}
+          xhr={{
+            withCredentials: false,
+            referrerPolicy: "strict-origin-when-cross-origin",
+          }}
         />
       </Box>
     </Box>
