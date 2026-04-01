6301231fc16c14b665ac7d023ede34770bd4d606
Ross Brown <albatrocity@gmail.com>
2026-03-31 19:55:49 -0500

    updates users with Google data on signin

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -50,6 +50,7 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       prompt: "select_account",
+      overrideUserInfoOnSignIn: true,
     },
   },
   plugins: [
